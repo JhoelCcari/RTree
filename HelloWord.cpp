6c1a5e9f35c51c3b3ea0b54be1b36a7a3154b416
Atipax <fraz0208@gmail.com>
2015-06-19 15:43:13 -0500

    Update HelloWord.cpp

--- HelloWord.cpp
+++ HelloWord.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 
 int main()
-{
+{ char prras;
   std::cout << "Hello world bitches .!! " << std::endl;
   //perrsss
   std::cout << "Hello world prssssss.!! ";
+  std::cout << "tong se la come doblada"<<std::endl;
+  std::cin >> prras;
   return 0;
 }
